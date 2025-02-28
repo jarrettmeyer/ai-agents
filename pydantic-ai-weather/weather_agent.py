76ec18b5a91ecf586bc8b10198d93a3d0b424334
Jarrett Meyer <meyer_jarrett@lilly.com>
2025-02-28 12:48:31 -0500

    code refactoring

--- pydantic-ai-weather/weather_agent.py
+++ pydantic-ai-weather/weather_agent.py
@@ -8,21 +8,16 @@ from devtools import debug
 from dotenv import load_dotenv
 from httpx import AsyncClient as AsyncHttpxClient
 from pydantic_ai import Agent, ModelRetry, RunContext
+from pydantic_ai.models.openai import OpenAIModel
 from typing import Any, Literal
 
 
 load_dotenv()
 
-# Get the location.
-try:
-    location = sys.argv[1]
-except IndexError:
-    print("\nusage: python weather_agent.py <location>\n")
-    sys.exit(1)
-
-
 @dataclass
 class WeatherDeps:
+    """Dependencies for the weather agent."""
+
     httpx_client: AsyncHttpxClient
     geocode_api_key: str
     geocode_endpoint: str
@@ -33,6 +28,8 @@ class WeatherDeps:
 
 @dataclass
 class LatLon:
+    """Latitude and longitude."""
+
     lat: float
     lon: float
 
@@ -45,10 +42,26 @@ with open("system_prompt.md") as f:
 with open("weather_codes.json") as f:
     weather_codes = json.load(f)
 
+llm_model = os.getenv("LLM_MODEL")
+if not llm_model:
+    raise ValueError("LLM_MODEL environment variable is required.")
+
+# If an OpenAI API key is set, then we will use OpenAI.
+# Otherwise, use Ollama running locally.
+if os.getenv("OPENAI_API_KEY"):
+    print(f"\nUsing OpenAI with {llm_model}.")
+    model = llm_model
+else:
+    ollama_host = os.getenv("OLLAMA_HOST", "http://localhost:11434/v1")
+    print(f"\nUsing Ollama at {ollama_host} with {llm_model}.")
+    model = OpenAIModel(
+        model_name=llm_model,
+        base_url=ollama_host,
+    )
+
 # Create the weather agent.
-# TODO: Use ollama running locally, instead of OpenAI.
 weather_agent = Agent(
-    model=os.getenv("LLM_MODEL", "openai:gpt-4o"),
+    model=model,
     system_prompt=system_prompt,
     deps_type=WeatherDeps,
     retries=2,
@@ -79,12 +92,13 @@ async def get_lat_lon(c: RunContext[WeatherDeps], address: str) -> LatLon:
     response = await c.deps.httpx_client.get(c.deps.geocode_endpoint, params=query_params)
     response.raise_for_status()
     data = response.json()
-    # debug(data)
 
     # TODO: Cache this result locally.
 
     if data:
-        return LatLon(lat=data[0]["lat"], lon=data[0]["lon"])
+        result = LatLon(lat=data[0]["lat"], lon=data[0]["lon"])
+        print(f"{address} => {result.lat}, {result.lon}")
+        return result
     else:
         raise ModelRetry(f"Could not find lat/lon for address '{address}'.")
 
@@ -126,20 +140,31 @@ async def get_weather(c: RunContext[WeatherDeps], lat: float, lon: float) -> Any
 def lookup_weather_code(c: RunContext[WeatherDeps], weather_code: int) -> str:
     """Lookup the friendly text description for a weather code.
 
+    This function is registered as a tool for the weather agent. The weather
+    code is returned by the get_weather tool as `weatherCode`.
+
     Example:
         lookup_weather_code(context, 1000) -> "Clear, Sunny"
-
-    This function is registered as a tool for the weather agent.
 
     Args:
         c: Run context.
         weather_code: Weather code to lookup.
     """
 
-    return weather_codes.get(str(weather_code), "Unknown")
+    weather_code_text = weather_codes.get("weatherCode", {}).get(str(weather_code), "Unknown")
+    print(f"weather code {weather_code} => {weather_code_text}")
+
+    return weather_code_text
 
 
 async def main():
+    # Get the location from the command line.
+    try:
+        location = sys.argv[1]
+    except IndexError:
+        print("\nusage: python weather_agent.py <location>\n")
+        return sys.exit(1)
+
     async with AsyncHttpxClient() as client:
         deps = WeatherDeps(
             httpx_client=client,
@@ -153,7 +178,8 @@ async def main():
         response = await weather_agent.run(f"What is the weather in {location}?", deps=deps)
         # debug(response)
 
-        print(f"\n\n{response.data}\n")
+        print(f"\n========================================")
+        print(f"\n{response.data}\n")
 
 
 if __name__ == "__main__":
